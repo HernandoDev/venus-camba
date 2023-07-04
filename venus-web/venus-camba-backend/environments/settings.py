62e9f12b08095f29f523c661263954664b63e7f6
HernandoDev <126403956+HernandoDev@users.noreply.github.com>
2023-07-03 22:36:18 -0400

    enviroments

--- venus-web/venus-camba-backend/environments/settings.py
+++ venus-web/venus-camba-backend/environments/settings.py
@@ -4,5 +4,5 @@ import os
 JWT_SECRET_KEY = 'super_secret'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/venuscambadb'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://venusdb:zFCSvrFtyAYT7@34.170.132.194:3306/venuscambadb'
 CORS_HEADERS = 'Content-Type'
